92fcbc6d2eb21fad57286cbdac01f6c9f64db49b
Aaron Lau <aaron-lau@users.noreply.github.com>
2024-08-23 00:20:22 -0400

    Aug2024 update (#59)
    * Use new Date() to render year in Footer
    
    * Add new svg files
    
    * Update Skills sections
    
    * Remove old svg files
    
    * Update experience history
    
    * Update landing section for AWS job
    
    * Update to gatsby 3.15.0
    
    ---------
    
    Co-authored-by: Aaron Lau <aaronlau@aarons-mbp-2.mynetworksettings.com>
    Co-authored-by: Aaron Lau <aaron-lau@aaron-lau.com>

--- src/components/portfolio/Skills.js
+++ src/components/portfolio/Skills.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
+
 import aws from '../../images/tech/aws.svg'
-import docker from '../../images/tech/docker.svg'
-import looker from '../../images/tech/looker.svg'
+import azure from '../../images/tech/azure.svg'
+import gcp from '../../images/tech/gcp.svg'
+import java from '../../images/tech/java.svg'
+import kubernetes from '../../images/tech/kubernetes.svg'
 import python from '../../images/tech/python.svg'
-import react from '../../images/tech/react.svg'
+import snowflake from '../../images/tech/snowflake.svg'
 import spark from '../../images/tech/spark.svg'
+import terraform from '../../images/tech/terraform.svg'
+
 
 import './skills.scss'
 
@@ -16,13 +21,18 @@ function Skills() {
       <h3 className="portfolio__sub-header">Technologies that I use currently</h3>
       <div className="g-3">
         <img className="skills__img" src={python} alt="Python" title="Python" />
-        <img className="skills__img" src={docker} alt="Docker" title="Docker" />
-        <img className="skills__img" src={react} alt="React" title="React" />
+        <img className="skills__img" src={java} alt="Java" title="Java" />
+        <img className="skills__img" src={spark} alt="Spark" title="Spark" />
       </div>
       <div className="g-3">
-      <img className="skills__img" src={spark} alt="Spark" title="Spark" />
-      <img className="skills__img" src={aws} alt="AWS" title="AWS" />
-        <img className="skills__img" src={looker} alt="Looker" title="Looker" />
+      <img className="skills__img" src={kubernetes} alt="Kubernetes" title="Kubernetes" />
+        <img className="skills__img" src={terraform} alt="Docker" title="Docker" />
+        <img className="skills__img" src={snowflake} alt="Snowflake" title="Snowflake" />
+      </div>
+      <div className="g-3">
+        <img className="skills__img" src={aws} alt="AWS" title="AWS" />
+        <img className="skills__img" src={gcp} alt="GCP" title="GCP" />
+        <img className="skills__img" src={azure} alt="Azure" title="Azure" />
       </div>
     </section>
   )

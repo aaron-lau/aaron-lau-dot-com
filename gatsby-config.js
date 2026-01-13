a0a221c140b4d9e925df883c5ec80b2e6cf28a8a
Aaron Lau <aaron.lau@clay.com>
2026-01-13 14:17:23 -0500

    chore: add .nvmrc and remove gatsby offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,8 +101,5 @@ module.exports = {
         trickleSpeed: 400,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    'gatsby-plugin-offline',
   ],
 }
